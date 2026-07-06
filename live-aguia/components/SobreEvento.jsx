3fc2c252bdcb6e3fa3db771d1cf4e0c3607a5917
Maicon Guizoni <dolinhoguizon@gmail.com>
2026-07-06 13:04:04 -0300

    agoravai2222

--- live-aguia/components/SobreEvento.jsx
+++ live-aguia/components/SobreEvento.jsx
@@ -1,10 +1,7 @@
 export default function SobreEvento() {
   return (
     <section className="mt-20 px-6 py-16 text-center text-white">
-
-      <div className="mx-auto mt-8 h-2 w-20 bg-[#ff9717]" />
-
-      <p className="mx-auto mt-10 max-w-6xl text-base leading-7 md:text-lg">
+      <p className="mx-auto mt-10 max-w-6xl text-lg leading-7 md:text-2xl">
         <strong>Com juros altos, escassez de novos terrenos e valorização acelerada no litoral catarinense, este pode ser o momento de transformar renda em patrimônio.</strong>
       </p>
     </section>
